3a5e4e32c3cd8304a9460201a11bed6c44530a54
aliiyuu <yu.alicia@gmail.com>
2026-01-17 11:56:58 -0800

    fix David Bohnett Foundation logo link

--- src/lib/components/sponsors.svelte
+++ src/lib/components/sponsors.svelte
@@ -9,13 +9,13 @@
 			href="https://mlh.link/MLH-PureButtons-hackathons" 
 			target="_blank" 
 			rel="noopener noreferrer"
-			class="sponsor-link w-full sm:w-1/2 md:w-1/4 p-4 flex items-center justify-center bg-white rounded-lg hover:bg-gray-200 transition-all duration-300"
+			class="sponsor-link w-56 h-40 p-4 flex items-center justify-center bg-white rounded-lg hover:bg-gray-200 transition-all duration-300"
 			aria-label="Visit Pure Buttons website (opens in a new tab)"
 		>
 			<img 
 				src="https://static.mlh.io/brand-assets/sponsors/pure-buttons/Pure-Buttons-Blue-Gradient-Logo-RGB.svg?_gl=1*19hc52h*_ga*Njc1MTgyNzI5LjE3NjcxMzU3MTM.*_ga_E5KT6TC4TK*czE3NjczMzUzNTckbzMkZzAkdDE3NjczMzUzNzAkajQ3JGwwJGgw" 
 				alt="Pure Buttons Logo" 
-				class="w-full h-auto max-h-24"
+				class="w-full h-full object-contain"
 			/>
 		</a>
 
@@ -24,13 +24,13 @@
 			href="https://www.bohnettfoundation.org/"
 			target="_blank" 
 			rel="noopener noreferrer"
-			class="sponsor-link w-full sm:w-1/2 md:w-1/4 p-4 flex items-center justify-center bg-white rounded-lg hover:bg-gray-200 transition-all duration-300"
+			class="sponsor-link w-56 h-40 p-4 flex items-center justify-center bg-white rounded-lg hover:bg-gray-200 transition-all duration-300"
 			aria-label="Visit David Bohnett Foundation website (opens in a new tab)"
 		>
 			<img 
-				src="https://uc5afa142a2390389f20cc6a4022.previews.dropboxusercontent.com/p/thumb/AC7TIO2qMEK_WNdvxDfPlvGcM0wTb2Yl6q0_OmsZVrzHwadwSMBcV7WoLWiqFie5PFPawWP-oEDz6KKPi39e_iqhnwh2zf1vC5wLst3JUo2XLnCcsADmgg6JaeqNrDHvLSOwivIFNnBzPfgTe90K2bdm1J-ThKx070xQE0zR3nOofAgFR9c3Oyffs3VrRxPHgml0UtbjXTVukJ4KBWKfduKNY89yfPq-py6M3R9c56eOjj_FiUOpEQUduWBFyk9VrXu0Bsym2S5L2wlKojG7SoMtvqmbXnZjACgRcraZ3B18oRvTmGzVEhOjY8Fe8WoMGy5JupktHAOXlkAW5VW5TFIh/p.png" 
+				src="https://lgbtq.ucla.edu/sites/default/files/styles/header_image/public/portals/38/images/headers/bohnett_foundation_logo.png?itok=1eU0gdDb" 
 				alt="David Bohnett Foundation Logo" 
-				class="w-full h-auto max-h-24"
+				class="w-full h-full object-contain"
 			/>
 		</a>
 	</div>
